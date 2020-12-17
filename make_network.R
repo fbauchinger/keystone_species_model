26677897fab57cad31fbd2f3e6dc75189d9dc050
Franziska Bauchinger <franziskabauchinger@gmail.com>
2020-12-17 11:15:43 +0100

    Update make_network.R

--- make_network.R
+++ make_network.R
@@ -8,7 +8,6 @@
 
 ### this script should be in the same directory as the .RData output from make_community.R
 
-
 ############################################################################
 ### INPUT PARAMETERS
 
@@ -36,6 +35,20 @@
 
 ## $sigCC = correlation coefficients between species (N x N matrix, N = number of species simulated; correlation type according to input parameter) 
 
+## $correlation.stats = statistics computed from the correlation network sigCC
+
+### network statistics computed are:
+#### rel.abun = mean relative abundance of a species across all sites
+#### cstr = sum of absolute correlation strengths of a species
+#### mean.cstr = mean of absolute correlation strengths of a species
+#### cb = betweenness centrality, calculated with igraph::betweenness
+#### cc = closeness centrality, calculated with igraph::closeness
+#### cc_rel = relative closeness centrality; normalized to the number of edges in the network
+#### md = node degree, calculated with graph::degree
+#### clust = clustering coefficient or local transitivity, calculated with igraph::transitivity with type=local
+#### eigen.centr = eigenvector centralities, calculated with igraph::eigen_centrality
+#### alpha.centr = alpha centrality, calculated with igraph:alpha_centrality
+#### transitivity = weighted transitivity, calculated with igraph::transitivity with type = "weighted"
 
 ############################################################################
 # USAGE:
@@ -46,6 +59,7 @@
 # load libraries
 library(permute)
 library(igraph)
+
 
 # compute pairwise correlations
 PairCor<-function(Ab, Cortype, alpha, sig, its) {
@@ -97,6 +111,7 @@ colnames(ex)<-c("target", "source", "strength", "alpha","Pvals","strength_abs", 
 list(network=ex, IntMat=alpha[PRES, PRES], sigCC= CorSig)
 }
 
+
 # calculate relative abundances from absolute abundance profiles
 rel.abundance<-function(Ab){
   Ab_temp<-Ab
@@ -108,6 +123,45 @@ rel.abundance<-function(Ab){
   return(Ab_temp)
 }
 
+
+make_active_net<-function(alpha, Ab, nNets, verb){
+  adj<-as.matrix(ifelse (alpha!=0 ,1,0))
+  g<-graph.adjacency(adj, mode="undirected")
+  t<-which(alpha!=0 ,arr.ind=T)
+  #reorder by row (just as igraph does obviously)
+  t<-t[ order(t[,1]),]
+  E(g)$color<-ifelse(alpha[t]>0,"green","red")
+  vcol<-matrix(ncol=ncol(Ab),nrow=nNets)
+  graphs<-list()
+  for(i in 1:nNets){
+    graphs[[i]]<-g
+    V(graphs[[i]])$color<-ifelse(Ab[i,]>0.01,1,0)
+    if(verb==T){plot(graphs[[i]])}
+    vcol[i,]<-as.numeric(V(graphs[[i]])$color)
+  }
+  list(graphs=graphs, Vcolor=vcol)
+}
+
+
+centrality.norm<-function(graph,type=c("degree","closeness","betweenness"),centralization=FALSE)
+{
+  result<-NA
+  g<-graph
+  cent<-centralization
+  if (!is.igraph(g)) {stop("Not a graph object")}
+  if (type[[1]] == "degree") {
+    if (!cent) result <- degree(gimode="all")/(vcount(g)-1)
+    else result <- (sum(max(degree(g,mode="all"))-degree(g,mode="all")))/((vcount(g)-1)*(vcount(g)-2))}
+  else if (type[[1]] == "betweenness") {
+    temp <- 2*betweenness(g,directed=F)/((vcount(g)-1)*(vcount(g)-2))
+    if (!cent) result <- temp
+    else  result <- sum(max(temp)-temp)/(vcount(g)-1)}
+  else if (type[[1]] == "closeness") {
+    if (!cent) result <- closeness(g,mode="all")
+    else result <- (2*vcount(g)-3)*(sum(max(closeness(g,mode="all"))-closeness(g,mode="all")))/((vcount(g)-1)*(vcount(g)-2))}
+  else {stop("this type is unavailable or mispelled")}
+  return(result)
+}
 
 
 
@@ -129,10 +183,33 @@ for (i in 1:length(InComm)) {
   basename=paste( substr(InComm[i],1,(nchar(InComm[i])-11)),"_c_",Cortype,"_its_",its,sep="")
   Ab<-rel.abundance(runA$Ab)
   Ab[is.finite(Ab)==FALSE]<-0
-  runB<-PairCor(alpha=runA$alpha, Ab=Ab, Cortype= Cortype, its=its,sig=0.05)
+  num.sites<-nrow(Ab)
+  runB<-PairCor(alpha=runA$alpha, Ab=Ab, Cortype=Cortype, its=its,sig=0.05)
 
+  Corr<-runB$sigCC
+  g<-make_active_net(Corr, Ab, num.sites, verb=F)
+  g2<-graph.adjacency(Corr,mode = "undirected", weighted = TRUE)
+  
+  cbtmp<-centrality.norm(g$graphs[[1]],type="betweenness",centralization=F)
+  cctmp<-centrality.norm(g$graphs[[1]],type="closeness",centralization=F)
+  cc_reltmp<-(centrality.norm(g$graphs[[1]],type="closeness",centralization=F))/max(centrality.norm(g$graphs[[1]],type="closeness",centralization=F))
+  mdtmp<-(degree(g$graphs[[1]])-2)
+  clusttmp<-transitivity(g$graphs[[1]],type="local")
+  eigencentr<-eigen_centrality(g$graphs[[1]],directed=TRUE,weights=NA)$vector
+  alphacentr<-alpha_centrality(g2)
+  trans<-transitivity(g2,type = "weighted")
+  cstr<-rowSums(abs(Corr))
+  mean.cstr<-rowMeans(abs(Corr))
+  
+  correlation.stats<-data.frame(OTU=rownames(Corr), rel.abun=colMeans(Ab),cstr=cstr,mean.cstr=mean.cstr,cb=cbtmp,cc=cctmp,cc.rel=cc_reltmp,
+                                md=mdtmp,clust=clusttmp,eigen.centr=eigencentr,alpha.centr=alphacentr,trans=trans)
+  
+  runB$correlation.stats<-correlation.stats
+  
   save(runB, file=paste(basename,"_network.RData",sep=""))
   
 }
+
+
 
 
